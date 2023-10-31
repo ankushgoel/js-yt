a93023ccc42e4a796c560a4ed69ccc419ff24bc3
Ankush Goel <ankushgoel@developersnation.com>
2023-10-31 19:40:32 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not broswer engine
+
+let age = 25 // number
+let name = "ankush" // string
+let isLoggedIn = true // boolean
+let state; // undefined
+
+// bigint
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniqueness
+
+// object
+
+console.log(typeof name); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
