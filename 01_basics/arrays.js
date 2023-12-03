69d5155a62403909fa5028351ad0736dab1d18e9
Ankush Goel <ankushgoel@developersnation.com>
2023-12-03 09:53:29 +0000

    multiple array manipulation

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -30,3 +30,28 @@ console.log("B ", myArr);
 
 console.log(myArr.splice(1, 3)); // modifies original array
 console.log("C ", myArr);
+
+const marvel = ["ironman", "thor", "spiderman"]
+const dc = ["superman", "flash", "batman"]
+
+// marvel.push(dc);
+
+// console.log(marvel);
+// console.log(marvel[3][1]);
+
+// const allHeros = marvel.concat(dc)
+
+const allHeros = [...marvel, ...dc]
+
+console.log(allHeros);
+
+const Arr = [1, 2, 3, [4, 5, 6], 7, [8, 9, [1, 0]]]
+
+console.log(Array.isArray("Ankush"));
+console.log(Array.from("Ankush"));
+console.log((Array.from({name : "Ankush"}))); // Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
