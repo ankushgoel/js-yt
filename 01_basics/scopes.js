c77fe7f2aa8054cc76beeb02cb7afdf71392a7fc
Ankush Goel <ankushgoel@developersnation.com>
2023-12-15 18:16:47 +0000

    scopes

---
+++ 01_basics/scopes.js
@@ -0,0 +1,31 @@
+let a = 10
+const b = 20
+var c = 30
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+if(true) {
+    let i = 10
+    const j = 20
+    var k = 30 
+
+    var c = 400
+    let a = 100
+    console.log("inside value of a: ", a)
+}
+
+// console.log(i)
+// console.log(j)
+console.log(k)
+
+console.log(c)
+console.log("outside value of a: ", a)
+
+array = [1, 2, 3, 4, 5, 6]
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
