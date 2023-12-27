bbaa4a9154fff5ba648b605a5c38144249876f35
Ankush Goel <ankushgoel@developersnation.com>
2023-12-27 16:59:53 +0000

    while, do-while loops

---
+++ 03_iterations.js/while.js
@@ -0,0 +1,20 @@
+let i = 0;
+
+while(i < 5) {
+    // console.log(i)
+    i++
+}
+
+const myArray = [2, 3, 5, 7, 9]
+let arr = 0
+while(arr < myArray.length) {
+    // console.log(myArray[arr])
+    arr++ // arr = arr + 1
+}
+
+
+let index = 11
+do {
+    console.log(index)
+    index++
+} while (index <= 5);
