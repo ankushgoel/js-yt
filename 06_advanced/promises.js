e5227a628705ff9169e664fde47977e5755345fc
Ankush Goel <ankushgoel@developersnation.com>
2024-02-10 17:50:57 +0000

    Async-await, fetch, promise.all

--- 06_advanced/promises.js
+++ 06_advanced/promises.js
@@ -8,7 +8,7 @@ const promiseOne = new Promise(function(resolve, reject){
 })
 
 promiseOne.then(function(){
-    console.log("Promise consumed");
+    console.log("PromiseOne consumed");
 })
 
 new Promise(function(resolve, reject){
@@ -33,7 +33,7 @@ promiseThree.then(function(user){
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true
+        let error = false
         if (!error) {
             resolve({username: "Ankush", password: "12345"})
         } else {
@@ -42,8 +42,8 @@ const promiseFour = new Promise(function(resolve, reject){
     }, 1000)
 })
 
- promiseFour
- .then((user) => {
+promiseFour
+.then((user) => {
     console.log(user);
     return user.username
 }).then((username) => {
@@ -51,3 +51,63 @@ const promiseFour = new Promise(function(resolve, reject){
 }).catch(function(error){
     console.log(error);
 }).finally(() => console.log("The promise is either resolved or rejected"))
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "Ankush", password: "12345"})
+        } else {
+            reject('ERROR: Something went wrong in five')
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+
+fetch('https://api.github.com/users/ankushgoel')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all()
+// This method accepts a single parameter iterable which takes an array of promises or a normal array 
+// that contains some objects.
+p1 = Promise.resolve(50);
+p2 = 200
+p3 = new Promise(function (resolve, reject) {
+    setTimeout(resolve, 100, 'geek');
+});
+ 
+Promise.all([p1, p2, p3]).then(function (values) {
+    console.log(values);
+});
+// Output
+// [ 50, 200, 'geek' ]
