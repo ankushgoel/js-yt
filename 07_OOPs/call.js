1cbdfc18a8a6d3b74cd0e5158f94191491b7cc28
Ankush Goel <ankushgoel@developersnation.com>
2024-02-13 18:26:36 +0000

    .call method

---
+++ 07_OOPs/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+   
+    this.email = email
+    this.password = password
+}
+
+const user = new createUser("ankush", "ankush@mail.com", "12345")
+console.log(user);
